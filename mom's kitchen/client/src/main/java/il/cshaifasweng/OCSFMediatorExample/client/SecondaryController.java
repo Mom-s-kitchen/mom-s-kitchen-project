fbdc14c53a4218a926cbf45ffa86ddc6b2a3821b
Saleh-Ra <saleh.rabah12@gmail.com>
2025-01-08 15:01:22 +0200

    Add files via upload

---
+++ mom's kitchen/client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SecondaryController.java
@@ -0,0 +1,10 @@
+package il.cshaifasweng.OCSFMediatorExample.client;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class SecondaryController {
+
+}
